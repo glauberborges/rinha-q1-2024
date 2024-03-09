e6d07eb1742fd980739fa1d3fbb20b44cf7dfa20
glauberborges <glauber.borges1@gmail.com>
2024-03-09 19:14:53 -0300

    .

--- src/config/autoload/logger.php
+++ src/config/autoload/logger.php
@@ -11,20 +11,20 @@ declare(strict_types=1);
  */
 return [
     'default' => [
-        'handler' => [
-            'class' => Monolog\Handler\StreamHandler::class,
-            'constructor' => [
-                'stream' => BASE_PATH . '/runtime/logs/hyperf.log',
-                'level' => Monolog\Logger::CRITICAL,
-            ],
-        ],
-        'formatter' => [
-            'class' => Monolog\Formatter\LineFormatter::class,
-            'constructor' => [
-                'format' => null,
-                'dateFormat' => 'Y-m-d H:i:s',
-                'allowInlineLineBreaks' => true,
-            ],
-        ],
+//        'handler' => [
+//            'class' => Monolog\Handler\StreamHandler::class,
+//            'constructor' => [
+//                'stream' => BASE_PATH . '/runtime/logs/hyperf.log',
+//                'level' => Monolog\Logger::EMERGENCY,
+//            ],
+//        ],
+//        'formatter' => [
+//            'class' => Monolog\Formatter\LineFormatter::class,
+//            'constructor' => [
+//                'format' => null,
+//                'dateFormat' => 'Y-m-d H:i:s',
+//                'allowInlineLineBreaks' => true,
+//            ],
+//        ],
     ],
 ];
